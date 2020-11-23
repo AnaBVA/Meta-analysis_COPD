96cae7d37a1aeada43c71ad647e5cabf1b91d8f5
AnaBVA <anabva2304@gmail.com>
2020-11-22 19:26:26 -0600

    fixing output file names

--- Blood/R/setup.R
+++ Blood/R/setup.R
@@ -3,18 +3,18 @@ library(stringr)
 TODAY = format(Sys.time(),'%Y-%m-%d')
 
 DATA <- function(x){
-  str_c(file.path(here::here("Blood/data/")),paste0("Blood ",x,collapse = ""))
+  str_c(file.path(here::here("Blood/data/")),paste0(x,collapse = ""))
 }
 
 OUTPUT <- function(x){
-  str_c(file.path(here::here("Blood/output_data/")),paste0("Blood ",x,collapse = ""))
+  str_c(file.path(here::here("Blood/output_data/")),"Blood_",paste0(x,collapse = ""))
 }
 
 DOWNLOAD <- function(x){
-  str_c(file.path(here::here("Blood/download/")),paste0("Blood ",x,collapse = ""))
+  str_c(file.path(here::here("Blood/download/")),"Blood_",paste0(x,collapse = ""))
 }
 
 FIG <- function(x){
-  str_c(file.path(here::here("Blood/fig/")),paste0("Blood ",x,collapse = ""))
+  str_c(file.path(here::here("Blood/fig/")),"Blood_",paste0(x,collapse = ""))
 }
 
